09b530989f9e88397850c2d1a1af98056a8979e9
Elonewolf1011 <sagarprem1011@gmail.com>
2024-05-07 09:49:42 +0530

    commit

--- nodejs/http.js
+++ nodejs/http.js
@@ -1,31 +1,29 @@
-//client side js
-
 let nameElement = document.querySelector('.name');
 let surnameElement = document.querySelector('.surname')
+let surnameInput = document.querySelector('#surnameInput');
 
-const getMyname = async () =>{
-    let res = await fetch('https://127.0.0.1:3000/')
+const getMyName = async () => {
+    let res = await fetch('http://127.0.0.1:3000/')
     console.log(res);
-    if (!res.ok) {
-        alert('failed to get name');
+    if(!res.ok) {
+        alert('Failed to get name');
         return;
     }
     let name = await res.text();
     nameElement.textContent = name;
 }
-
-
-const postsurname = async () => {
-    let res = await fetch('https://127.0.0.1:3000/',{
-        method: 'post',
+const postSurname = async () => {
+    let mysurname = surnameInput.value;
+    let res = await fetch('http://127.0.0.1:3000/', {
+        method: 'POST',
         headers: {
-            'content-type':'application/json'
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify({surname:'kumar'})
+        body: JSON.stringify({surname: mysurname})
     })
-    console.log(res);
+    // console.log(res);
     if(!res.ok) {
-        alert('failed to post surname');
+        alert('Failed to post surname');
         return;
     }
     let { surname } = await res.json();
